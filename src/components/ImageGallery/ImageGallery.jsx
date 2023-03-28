69d6fc95a97964754c07131b2d6b382fac9ade12
ABerseneva <dirtydonut@icloud.com>
2023-03-28 09:30:07 +0200

    searchbar refactoring

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from '../ImageGallery/ImageGallery.module.css';
 import PropTypes from 'prop-types';
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
 
-export class ImageGallery extends Component {
-  render() {
-    return (
-      <>
-        <ul className={css.gallery}>
-          {this.props.images.map(image => (
-            <ImageGalleryItem
-              key={image.id}
-              imageURL={image.webformatURL}
-              alt={image.tags}
-              largeIMG={image.largeImageURL}
-            />
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
+export const ImageGallery = ({ images }) => {
+  return (
+    <>
+      <ul className={css.gallery}>
+        {images.map(image => (
+          <ImageGalleryItem
+            key={image.id}
+            imageURL={image.webformatURL}
+            alt={image.tags}
+            largeIMG={image.largeImageURL}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default ImageGallery;
 
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,53 +1,46 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import css from '../Searchbar/Searchbar.module.css';
 import PropTypes from 'prop-types';
 
-export class Searchbar extends Component {
-  state = {
-    searchString: '',
+function Searchbar({ onSubmit }) {
+  const [searchString, setSearchString] = useState('');
+
+  const handleChange = event => {
+    setSearchString(event.currentTarget.value.toLowerCase());
   };
 
-  handleChange = event => {
-    this.setState({
-      searchString: event.currentTarget.value.toLowerCase(),
-    });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    if (!this.state.searchString) {
+    if (!searchString) {
       toast.warn('Fill in the field!');
       return;
     }
-    this.props.onSubmit(this.state.searchString);
-    this.setState({ searchString: '' });
+    onSubmit(searchString);
+    setSearchString('');
   };
 
-  render() {
-    const { searchString } = this.state;
-    return (
-      <header className={css.searchbar}>
-        <form className={css.form} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.button}>
-            <span className={css.button_label}>Search</span>
-          </button>
+  return (
+    <header className={css.searchbar}>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <button type="submit" className={css.button}>
+          <span className={css.button_label}>Search</span>
+        </button>
 
-          <input
-            onChange={this.handleChange}
-            value={searchString}
-            className={css.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          onChange={handleChange}
+          value={searchString}
+          className={css.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
 }
 
 export default Searchbar;
@@ -55,3 +48,55 @@ export default Searchbar;
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+// export class Searchbar extends Component {
+//   state = {
+//     searchString: '',
+//   };
+
+//   handleChange = event => {
+//     this.setState({
+//       searchString: event.currentTarget.value.toLowerCase(),
+//     });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+
+//     if (!this.state.searchString) {
+//       toast.warn('Fill in the field!');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.searchString);
+//     this.setState({ searchString: '' });
+//   };
+
+//   render() {
+//     const { searchString } = this.state;
+//     return (
+//       <header className={css.searchbar}>
+//         <form className={css.form} onSubmit={this.handleSubmit}>
+//           <button type="submit" className={css.button}>
+//             <span className={css.button_label}>Search</span>
+//           </button>
+
+//           <input
+//             onChange={this.handleChange}
+//             value={searchString}
+//             className={css.input}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Searchbar;
+
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
